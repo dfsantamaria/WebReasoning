44925c38c55f831cf6232a11958aee723cecacfc
dfsantamaria <santamaria@dmi.unict.it>
2018-03-22 16:46:04 +0100

    Project Started

---
+++ src/main/java/com/webreasoning/exercise/owlapiex/main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.webreasoning.exercise.owlapiex;
+import org.semanticweb.owlapi.model.OWLOntology;
+/**
+ *
+ * @author Daniele Francesco Santamaria 
+ */
+public class main
+  {
+   public static void main(String[] args) 
+    {
+          
+    }
+  }
