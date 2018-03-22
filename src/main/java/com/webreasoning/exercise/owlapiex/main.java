d6c52dc5df7ec0b7295ad1886ffb7eb2eb59d76e
dfsantamaria <santamaria@dmi.unict.it>
2018-03-22 18:22:17 +0100

    Project Started

--- src/main/java/com/webreasoning/exercise/owlapiex/main.java
+++ src/main/java/com/webreasoning/exercise/owlapiex/main.java
@@ -7,6 +7,7 @@ package com.webreasoning.exercise.owlapiex;
 import java.io.File;
 import java.io.FileNotFoundException;
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
@@ -19,6 +20,9 @@ public class main
    public static void main(String[] args) throws FileNotFoundException, OWLOntologyCreationException
     {
        OWLOntologyManager manager= OWLManager.createOWLOntologyManager(); //create the manager    
-       OWLOntology ontology= ontology=manager.loadOntologyFromOntologyDocument(new File("ontologie/E1G1.owl"));
+       OWLOntology ontology=manager.loadOntologyFromOntologyDocument(new File("ontologie/E1G1.owl"));
+    /* String iri="http://www.w3.org/2009/08/skos-reference/skos.rdf";
+     IRI docIRI = IRI.create(iri); 
+     OWLOntology ontology = manager.loadOntologyFromOntologyDocument(docIRI);*/
     }
   }
