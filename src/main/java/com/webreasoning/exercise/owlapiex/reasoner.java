bed53959680918ae346fcdcccded43e82785603d
dfsantamaria <santamaria@dmi.unict.it>
2018-04-09 17:12:02 +0200

    Project Started

--- src/main/java/com/webreasoning/exercise/owlapiex/reasoner.java
+++ src/main/java/com/webreasoning/exercise/owlapiex/reasoner.java
@@ -1,6 +1,7 @@
 package com.webreasoning.exercise.owlapiex;
 import java.io.File;
 import java.io.FileNotFoundException;    
+import openllet.owlapi.PelletReasonerFactory;
 
 import org.semanticweb.HermiT.Configuration;
 import org.semanticweb.owlapi.apibinding.OWLManager;
@@ -10,11 +11,14 @@ import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 import org.semanticweb.HermiT.Reasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
 /**
  *
  * @author Daniele Francesco Santamaria 
  */
-public class query
+public class reasoner
   {
    public static void main(String[] args) throws FileNotFoundException, OWLOntologyCreationException, OWLOntologyStorageException
      {
@@ -24,13 +28,22 @@ public class query
     /* String iri="http://www.w3.org/2009/08/skos-reference/skos.rdf";
      IRI docIRI = IRI.create(iri); 
      OWLOntology ontology = manager.loadOntologyFromOntologyDocument(docIRI);*/
+   
+    /*
     OWLDataFactory datafact=manager.getOWLDataFactory();
     Configuration config= new Configuration();
     Reasoner reasoner= new Reasoner(config, ontology);
     reasoner.classifyClasses();
     reasoner.classifyDataProperties();
     reasoner.classifyObjectProperties();
-    System.out.println(reasoner.isConsistent());  
+    System.out.println(reasoner.isConsistent());  */
     
+    
+    OWLReasonerFactory reasonerFactoryP = PelletReasonerFactory.getInstance();
+    OWLReasoner reasonerP = reasonerFactoryP.createReasoner(ontology, new SimpleConfiguration());    
+    reasonerP.precomputeInferences();
+    System.out.println(reasonerP.isConsistent());
+    
+   
   }
   }
