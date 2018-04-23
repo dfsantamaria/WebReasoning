d2b0d9495656b8817e330cad6a1c0ba4ef55bdd9
dfsantamaria <santamaria@dmi.unict.it>
2018-04-23 17:31:45 +0200

    Project Started

---
+++ src/main/java/com/webreasoning/exercise/ontapi/MarriedTwo.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.webreasoning.exercise.ontapi;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.util.FileManager;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
+
+/**
+ *
+ * @author mariannanicolosi
+ */
+public class MarriedTwo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // TODO code application logic here
+        
+        String mr3URI = "http://mr3.sourceforge.net#"; // URI da utilizzare per le risorse dell'ontologia.  
+        
+        //String inputFileName="D:" + File.separator + "Dati" + File.separator + "Documents"  + File.separator + 
+                             //"NetBeansProjects"+ File.separator + "ONTAPI"+File.separator+"esempioJ.rdf";
+        String PATH = File.separator + "Users" + File.separator + "mariannanicolosi"  + File.separator + 
+                             "Desktop"+ File.separator + "Lavoro"+File.separator+"CorsoWebReasoning"+File.separator+
+                             "Lezioni201718"+File.separator+"EsempiLezione8"+File.separator;                   
+        String inputFileName= "MarriedWJena2.rdf";
+        
+        // create an empty model
+        Model model = ModelFactory.createDefaultModel();
+        // use the FileManager to find the input file
+        InputStream in = FileManager.get().open(inputFileName);
+         if (in == null) {
+                    throw new IllegalArgumentException(
+                                 "File: " + inputFileName + " not found");
+         }
+         // read the RDF/XML file
+         model.read(in, null);
+         // write it to standard out
+         
+         model.write(System.out);
+         //Valutiamo la seguente query SPARQL
+         in.close();
+         //Valutiamo la seguente query SPARQL
+         System.out.println("Esecuzione della seconda query.");
+         String queryString  = "PREFIX mr3: <"+mr3URI+">"+
+                               "PREFIX rdfs: <"+RDFS.getURI()+">"+
+                               "PREFIX rdf: <"+RDF.getURI()+">"+
+                               "CONSTRUCT {mr3:hasMaidenName rdfs:domain ?C."+
+                                         "?individual rdf:type ?C}" +
+                              "WHERE {?individual mr3:hasMaidenName ?name."+
+                                       "mr3:hasMaidenName rdfs:domain ?D." +
+                                       "?D rdfs:subClassOf ?C ."
+                              + "}" ;
+         
+          
+         Query query = QueryFactory.create(queryString); //Creiamo la query.         
+         QueryExecution qexec = QueryExecutionFactory.create(query, model);    
+         
+         Model m = qexec.execConstruct();  
+         System.out.println("Risultato di construct = " + m.toString());
+         
+//       
+         System.out.println("Risultato di construct = " + m.toString());
+         model.close();
+//         qexec.close(); 
+    }
+    
+}
